794fcefe4f71b6bf1bf4eb8fac98af603f03f0cd
ThreeCoffees <leonard.madzielewski@gmail.com>
2026-05-04 17:34:03 +0200

    feat: detach from wall when stunned

--- src/shared/health/stunnable_component.gd
+++ src/shared/health/stunnable_component.gd
@@ -1,9 +1,8 @@
 class_name StunnableComponent
 extends Node
 
-@export var hitbox: HitBox
 @export var player_physics: PlayerPhysics 
-
+@export var gravity_controller: GravityController 
 
 func _on_hit_box_entered(node: Node) -> void:
 	var stun_component = node.find_children("", "StunComponent").get(0) as StunComponent
@@ -13,5 +12,10 @@ func _on_hit_box_entered(node: Node) -> void:
 
 	var prev_speed = player_physics.speed
 	player_physics.speed *= stun_component.speed_multiplier
+	gravity_controller.are_sensors_locked = true
+	gravity_controller.are_sensors_active = false
+
 	await get_tree().create_timer(stun_component.stun_duration).timeout
+
+	gravity_controller.are_sensors_locked = false
 	player_physics.speed = prev_speed
