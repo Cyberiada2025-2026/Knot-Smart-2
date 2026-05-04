664ab9329741e0cbbc3d073e325adba8e9ff1d4a
ThreeCoffees <leonard.madzielewski@gmail.com>
2026-05-04 20:46:50 +0200

    wip

--- src/ui/main_menu/main_menu.gd
+++ src/ui/main_menu/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_button_pressed():
-	SceneManager.goto_scene("res://scenes/main/main.tscn")
+	SceneManager.goto_scene("res://scenes/debug/beta_level/beta_level.tscn")
 
 
 func _on_quit_button_pressed() -> void:
