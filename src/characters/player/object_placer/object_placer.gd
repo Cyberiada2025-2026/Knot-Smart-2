5680ce4c9089873ddc5993a6b04acc2f09b67f47
Titanic472 <ctac472@gmail.com>
2026-05-24 11:52:56 +0200

    placer positioning fixes

--- src/characters/player/object_placer/object_placer.gd
+++ src/characters/player/object_placer/object_placer.gd
@@ -8,12 +8,16 @@ signal placement_finished(placed_object: Node3D)
 
 enum State { IDLE, SELECTING_POSITION }
 
-## maximun radius around player where placement is allowed
+## maximum radius around player where placement is allowed
 @export var placement_range: float = 3
+## maximum height difference between player and placed object
+@export var max_height_difference: float = 3
 ## maximum surface angle in degrees which allows teleporter placement
 @export var max_placement_angle = 20
 ## rotation speed in degrees per second
 @export var rotation_speed: int = 90
+
+const ADDITIONAL_RAYCAST_HEIGHT = 10
 
 var _state = State.IDLE
 
@@ -21,6 +25,7 @@ var _marker_rotation
 var _prev_mouse_mode
 var _prev_camera_mode
 var _camera: PlayerCamera
+var _player_position: Vector3
 
 var _item_to_be_placed
 
@@ -92,38 +97,22 @@ func _physics_process(delta: float) -> void:
 	if raycast_result.is_empty():
 		return
 
-	var player_position = get_node("../").player_physics.global_position
+	_player_position = get_node("../").player_physics.global_position
 
-	if _v3_to_v2(player_position).distance_to(_v3_to_v2(raycast_result.position)) > placement_range:
+	#if _v3_to_v2(player_position).distance_to(_v3_to_v2(raycast_result.position)) > placement_range:
+	#if player_position.distance_to(raycast_result.position) > placement_range:
+
+	if is_position_out_of_range(raycast_result.position):
 		raycast_result.position = (
-			(raycast_result.position - player_position).normalized() * placement_range
-			+ player_position
+			(raycast_result.position - _player_position).normalized() * placement_range
+			+ _player_position
 		)
-		raycast_result.position.y = player_position.y
+		raycast_result.position.y = _player_position.y
 
-		var roof = (
-			UnsafeRaycastBuilder.new(self)
-				.set_ray_length(50)
-				.set_raycast_origin(raycast_result.position)
-				.set_direction(Vector3.UP)
-				.set_collision_mask(1)
-				.raycast()
-		)
+	raycast_result = roof_checking_raycast(raycast_result.position)
 
-		var raycast_origin = raycast_result.position + Vector3(0, 50, 0)
-
-		if not roof.is_empty():
-			raycast_origin.y = roof.position.y
-
-		raycast_result = (
-			UnsafeRaycastBuilder.new(self)
-				.set_ray_length(100)
-				.set_raycast_origin(raycast_origin)
-				.set_direction(Vector3.DOWN)
-				.raycast()
-		)
-		if raycast_result.is_empty():
-			return
+	if raycast_result.is_empty() or abs(_player_position.y - raycast_result.position.y) > max_height_difference:
+		return
 
 	# avoiding too big terrain angles
 	var hit_normal = raycast_result.normal
@@ -131,25 +120,7 @@ func _physics_process(delta: float) -> void:
 	if rad_to_deg(slope_angle_rad) > max_placement_angle:
 		return
 
-	# fix box height to avoid being in textures
-	raycast_result.position += _marker.get_half_height() * raycast_result.normal
-
-	_marker.quaternion = Quaternion(Vector3.UP, raycast_result.normal)
-	_marker.global_position = raycast_result.position
-	# fix collision with floor due to low float precision
-	_marker.global_position += Vector3(0, 0.000001, 0)
-
-
-	if Input.is_action_pressed("rotate"):
-		_marker_rotation = _marker_rotation + rotation_speed * delta
-	if _marker_rotation > 360:
-		_marker_rotation -= 360
-
-	_marker.rotate_object_local(Vector3.UP, deg_to_rad(_marker_rotation))
-
-	_marker.update_state()
-
-	_marker.show()
+	update_marker(raycast_result, delta)
 
 	if Input.is_action_just_pressed("left_mouse") and _marker.allows_placement:
 		_place()
@@ -168,5 +139,57 @@ func _place():
 	placement_finished.emit(placed_item)
 
 
+func is_position_out_of_range(_position: Vector3):
+	#return _player_position.distance_to(_position) > placement_range
+	return _v3_to_v2(_player_position).distance_to(_v3_to_v2(_position)) > placement_range
+
+
 func _v3_to_v2(vector: Vector3) -> Vector2:
 	return Vector2(vector.x, vector.z)
+
+
+func roof_checking_raycast(_position: Vector3):
+	var roof = (
+			UnsafeRaycastBuilder.new(self)
+				.set_ray_length(ADDITIONAL_RAYCAST_HEIGHT)
+				.set_raycast_origin(_position)
+				.set_direction(Vector3.UP)
+				.set_collision_mask(1)
+				.raycast()
+		)
+
+	var raycast_origin = _position + Vector3(0, ADDITIONAL_RAYCAST_HEIGHT, 0)
+
+	if not roof.is_empty():
+		raycast_origin.y = roof.position.y
+
+	var raycast_result = (
+		UnsafeRaycastBuilder.new(self)
+			.set_ray_length(ADDITIONAL_RAYCAST_HEIGHT * 2)
+			.set_raycast_origin(raycast_origin)
+			.set_direction(Vector3.DOWN)
+			.raycast()
+	)
+	return raycast_result
+
+
+func update_marker(raycast_result, delta):
+	# fix box height to avoid being in textures
+	raycast_result.position += _marker.get_half_height() * raycast_result.normal
+
+	_marker.quaternion = Quaternion(Vector3.UP, raycast_result.normal)
+	_marker.global_position = raycast_result.position
+	# fix collision with floor due to low float precision
+	_marker.global_position += Vector3(0, 0.000002, 0)
+
+
+	if Input.is_action_pressed("rotate"):
+		_marker_rotation = _marker_rotation + rotation_speed * delta
+	if _marker_rotation > 360:
+		_marker_rotation -= 360
+
+	_marker.rotate_object_local(Vector3.UP, deg_to_rad(_marker_rotation))
+
+	_marker.update_state()
+
+	_marker.show()
