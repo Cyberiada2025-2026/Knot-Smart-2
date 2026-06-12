5afac136643b86e6d3f6d25d291e8545043c414a
sajmon <szymon17099@gmail.com>
2026-06-12 12:06:11 +0200

    Add iterative decimate

--- src/shared/buildings/decimate_building/decimate_building.gd
+++ src/shared/buildings/decimate_building/decimate_building.gd
@@ -6,22 +6,25 @@ extends Node3D
 @export_tool_button("Decimate") var execute_btn = execute_decimate
 
 @export_range(0.0, 1.0) var removal_chance = 0.0
+@export var iterations = 1
 
 var random = RandomNumberGenerator.new()
 
 
 func execute_decimate():
 	random.set_seed(2137)
+
+	for _i in range(iterations):
+		decimate()
+
+
+func decimate():
 	var building = get_parent().get_node("GeneratedBuilding")
 	var gridmaps = []
 	for child in building.get_children():
 		if child is GridMap:
 			gridmaps.append(child)
-
-	decimate(gridmaps)
-
-
-func decimate(gridmaps):
+	
 	var cells = gridmaps.reduce(func(acc, gridmap): return acc + gridmap.get_used_cells(), [])
 	cells.sort_custom(func(v1, v2): return v1.y > v2.y)
 
