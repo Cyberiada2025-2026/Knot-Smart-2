dc33f0c66a4a9b5907fa223730705860918fd9a5
Kovilli <s203283@student.pg.edu.pl>
2026-06-12 15:15:39 +0200

    add health bar texture

--- src/ui/health/health.gd
+++ src/ui/health/health.gd
@@ -4,24 +4,22 @@ var player: Node
 var health_component: HealthComponent
 var max_health: float
 var health_bar: Control
+var health: float
 
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
 	health_component = player.get_node("HealthComponent")
-	health_bar = get_node("missing_health/current_health")
+	health_bar = get_child(0)
 	max_health = health_component.max_health
+	health = health_component.health
 	health_component.health_decreased.connect(health_changed)
 	health_component.health_increased.connect(health_changed)
 	health_component.max_health_changed.connect(max_health_changed)
 
 
 func health_changed() -> void:
-	var bar_size: Vector2
-	var original_bar_size = get_node("missing_health").get_size()
-	bar_size.y = original_bar_size.y
-	bar_size.x = (health_component.health / max_health) * original_bar_size.x
-	health_bar.set_size(bar_size)
+	health_bar.value = health / max_health
 
 
 func max_health_changed() -> void:
