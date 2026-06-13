e334333e693c614e44032679c421ea59753a1ce2
Kovilli <s203283@student.pg.edu.pl>
2026-06-13 20:48:53 +0200

    fix health

--- src/ui/health/health.gd
+++ src/ui/health/health.gd
@@ -13,14 +13,21 @@ func _ready() -> void:
 	health_bar = get_child(0)
 	max_health = health_component.max_health
 	health = health_component.health
+	health_changed()
 	health_component.health_decreased.connect(health_changed)
 	health_component.health_increased.connect(health_changed)
 	health_component.max_health_changed.connect(max_health_changed)
 
 
-func health_changed() -> void:
-	health_bar.value = health / max_health
+func change_bar_size():
+	health_bar.value = health / max_health * 100
 
 
-func max_health_changed() -> void:
+func health_changed():
+	health = health_component.health
+	change_bar_size()
+
+
+func max_health_changed():
 	max_health = health_component.max_health
+	change_bar_size()
