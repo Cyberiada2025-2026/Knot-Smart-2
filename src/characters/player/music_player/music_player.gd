06de0cdb8f8f1c7b64596d9ec0f293da96599acd
Titanic472 <ctac472@gmail.com>
2026-06-13 14:39:46 +0200

    cross-fade works

--- src/characters/player/music_player/music_player.gd
+++ src/characters/player/music_player/music_player.gd
@@ -6,56 +6,40 @@ extends AudioStreamPlayer
 @export var fade_out_duration: float = 1
 @export var volume: float = 0.0
 
-const MIN_VOLUME = -20
+const MIN_VOLUME = -40
 
 var fade_tween = null
-var interrupt: bool = false
 
-#var is_waiting = false
-
-func _ready() -> void:
-	volume_db = MIN_VOLUME
-	
-
-func _process(delta: float) -> void:
-	print(volume_db)
-	
 	
 func set_soundtrack(soundtrack: AudioStream):
 	sync_tweens()
-	
-	#await get_tree().process_frame
 	
 	if soundtrack == null:
 		fade_out()
 		return
 	
 	if soundtrack != self.stream:
+		var cross_player = self.duplicate()
+		add_child(cross_player)
+		cross_player.play(self.get_playback_position())
+		cross_player.fade_and_die()
+		
+		self.volume_db = MIN_VOLUME
 		self.set_stream(soundtrack)
 		self.play()
 	
 	fade_in()
 
 
-#func reset_soundtrack():
-	##while is_waiting:
-		##await get_tree().create_timer(0.01).timeout
-	#
-	#if volume_db != MIN_VOLUME:
-		#await fade_out()
-	#self.stop()
-	#self.set_stream(null)
-
-
 func change_volume(value: float) -> void:
 		volume_db = value
 
 
-func fade_out(vol = MIN_VOLUME, duration = fade_out_duration) -> void:
-	if volume_db == vol:
+func fade_out() -> void:
+	if volume_db == MIN_VOLUME:
 		return
 	fade_tween = create_tween().bind_node(self)
-	fade_tween.tween_method(change_volume, volume_db, MIN_VOLUME, duration)
+	fade_tween.tween_method(change_volume, volume_db, MIN_VOLUME, fade_out_duration)
 	await fade_tween.finished
 
 
@@ -70,3 +54,8 @@ func sync_tweens():
 		fade_tween.kill()
 		#fade_tween.free()
 		fade_tween = null
+		
+
+func fade_and_die():
+	await fade_out()
+	self.queue_free()
