74a2695a02d648a0c37266078ecdd623e540f8d4
ThreeCoffees <leonard.madzielewski@gmail.com>
2026-06-14 21:35:12 +0200

    protect boss when sleeping

--- src/characters/enemies/big_boss/big_boss.gd
+++ src/characters/enemies/big_boss/big_boss.gd
@@ -6,15 +6,18 @@ extends AnimatableBody3D
 @export_file_path(".tscn") var lose_scene 
 @export var boss_model: Node3D
 @export var battle_soundtrack: AudioStream
+@export var health_component: HealthComponent
 var animation_player: AnimationPlayer
 
 
 func _ready() -> void:
 	animation_player = boss_model.find_child("AnimationPlayer")
 	animation_player.play("Laying")
+	health_component.is_protected = true
 
 
 func _on_start_boss_battle() -> void:
+	health_component.is_protected = false
 	animation_player.play("Walk")
 	movement_animation.play("move_to_gen")
 	var music_player = get_tree().get_first_node_in_group("Player").get_node("MusicPlayer") as MusicPlayer
--- src/shared/health/health_component.gd
+++ src/shared/health/health_component.gd
@@ -19,6 +19,8 @@ signal health_depleted
 
 @export var health: float = 10.0:
 	set(value):
+		if is_protected:
+			return
 		var prev_health = health
 		health = clamp(value, 0, max_health)
 		if debug_log:
@@ -36,3 +38,5 @@ signal health_depleted
 			if debug_log:
 				print("Health depleted")
 			health_depleted.emit()
+
+@export var is_protected: bool = false
